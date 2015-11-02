d17818c04f81e140a0a06b2bdf97bec7f4313040
MKS-students <mks.students@hackreactor.com>
2015-11-02 15:29:13 -0600

    solution for each

--- src/underbar.js
+++ src/underbar.js
@@ -50,6 +50,16 @@
   // Note: _.each does not have a return value, but rather simply runs the
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
+    if(Array.isArray(collection)){
+      for(var i = 0; i < collection.length; i++){
+        iterator(collection[i], i, collection);
+      }
+    }
+    else {
+      for(var key in collection) {
+        iterator(collection[key], key, collection);
+      }
+    }
 
   };
 
