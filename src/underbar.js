e080354679b65c2e497b0be5d29b60844963495f
MKS-students <mks.students@hackreactor.com>
2015-11-03 19:13:54 -0600

    completed some

--- src/underbar.js
+++ src/underbar.js
@@ -204,8 +204,13 @@
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
-  _.some = function(collection, iterator) {
+  _. some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
+    if (iterator === undefined) { iterator = _.identity }
+       return !_.every(collection, function(item) {
+      return !iterator(item);
+    })
+
   };
 
 
